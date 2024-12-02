29a1466927e978c9a19601086ad03afbb0149566
jmhung6 <albert0426@gmail.com>
2024-12-02 20:57:57 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -4,7 +4,7 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 
 def send_email():
     # 從 GitHub Secrets 中獲取 SendGrid API 密鑰
-    sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
+    sendgrid_api_key = os.getenv('FirstApiKey')
 
     if not sendgrid_api_key:
         print("SENDGRID_API_KEY is not set.")
@@ -12,8 +12,8 @@ def send_email():
 
     sg = sendgrid.SendGridAPIClient(api_key=sendgrid_api_key)
 
-    from_email = Email("your-email@example.com")  # 你的發件人郵件地址
-    to_email = To("recipient@example.com")  # 收件人郵件地址
+    from_email = Email("a253.tw@yahoo.com.tw")  # 你的發件人郵件地址
+    to_email = To("a253.tw@yahoo.com.tw")  # 收件人郵件地址
     subject = "Test Email from GitHub Actions"
     content = Content("text/plain", "This is a test email sent from GitHub Actions.")
 
